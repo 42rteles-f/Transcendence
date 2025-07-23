022e918fb9b0d69f2f58f0a4f33a0b0fe4db7c1b
rtfjloq-linux <tfj.rubens@gmail.com>
2025-07-23 22:07:11 +0100

    implemented pong method update on parent as well. implemented paddle class position and update

--- frontend/public/pages/chat/chat.ts
+++ frontend/public/pages/chat/chat.ts
@@ -4,115 +4,123 @@ import Socket from "../../../src/Socket";
 console.log("executing home.ts");
 
 class Chat extends BaseComponent {
-	private chatMessages!: HTMLDivElement;
-	private chatInput!: HTMLInputElement;
-	private sendButton!: HTMLButtonElement;
-	private clientList!: HTMLUListElement;
-	private chatName!: HTMLDivElement;
+    private chatMessages!: HTMLDivElement;
+    private chatInput!: HTMLInputElement;
+    private sendButton!: HTMLButtonElement;
+    private clientList!: HTMLUListElement;
+    private chatName!: HTMLDivElement;
 
-	private chatHistory: Map<string, HTMLDivElement[]> = new Map();
+    private chatHistory: Map<string, HTMLDivElement[]> = new Map();
 
-	public	messageLimit: number = 5;
+    public messageLimit: number = 5;
 
-	constructor() {
-		super("/pages/chat/chat.html");
-	}
+    constructor() {
+        super("/pages/chat/chat.html");
+    }
 
-	override onInit(): void {
-		console.log("Chat component initialized");
-		this.sendButton.onclick = () => this.sendMessage();
-		this.chatInput.onkeydown = (e: KeyboardEvent) => {
-			if (e.key === "Enter") this.sendMessage();
-		};
-		Socket.init();
-		Socket.addEventListener("chat-message", this.addMessage);
-		Socket.addEventListener("client-arrival", this.addClients);
-		Socket.addEventListener("client-departure", this.removeClient);
-		Socket.addEventListener("disconnect", () => {
-			this.clientList.innerHTML = "";
-		});
-	}
+    override onInit(): void {
+        console.log("Chat component initialized");
+        this.sendButton.onclick = () => this.sendMessage();
+        this.chatInput.onkeydown = (e: KeyboardEvent) => {
+            if (e.key === "Enter") this.sendMessage();
+        };
+        Socket.init();
+        Socket.addEventListener("chat-message", this.addMessage);
+        Socket.addEventListener("client-arrival", this.addClients);
+        Socket.addEventListener("client-departure", this.removeClient);
+        Socket.addEventListener("disconnect", this.disconnect);
+    }
 
-	sendMessage() {
-		const message = this.chatInput.value.trim();
-		if (!message) return ;
+    sendMessage() {
+        const message = this.chatInput.value.trim();
+        if (!message) return;
 
-		Socket.emit("chat-message", {
-			target: this.chatName.dataset.id,
-			message: message
-		});
-		this.addMessage({fromId: "self", fromName: this.chatName.textContent!, message}, "outgoing");
-		this.chatInput.value = '';
-	}
+        Socket.emit("chat-message", {
+            target: this.chatName.dataset.id,
+            message: message,
+        });
+        this.addMessage(
+            { fromId: "self", fromName: this.chatName.textContent!, message },
+            "outgoing"
+        );
+        this.chatInput.value = "";
+    }
 
-	addMessage = (
-		data: { fromId: string, fromName: string, message: string },
-		type?: "incoming" | "outgoing"
-	): void => {
-		if (type !== "outgoing") type = "incoming";
-		console.log(`chatName (${this.chatName.textContent}) != fromName (${data.fromName}) && type (${type})`);
-		
-		const messageElement = document.createElement("div");
-		messageElement.className = `chat-message-${type}`;
-		messageElement.textContent = data.message;
+    disconnect = () => {
+        this.clientList.innerHTML = "";
+    };
 
-		if (!this.chatHistory.has(data.fromName)) this.chatHistory.set(data.fromName, []);
-		this.chatHistory.get(data.fromName)!.push(messageElement);
+    addMessage = (
+        data: { fromId: string; fromName: string; message: string },
+        type?: "incoming" | "outgoing"
+    ): void => {
+        if (type !== "outgoing") type = "incoming";
+        console.log(
+            `chatName (${this.chatName.textContent}) != fromName (${data.fromName}) && type (${type})`
+        );
 
-		if (this.chatName.textContent != data.fromName && type != "outgoing") return ;
-		this.chatMessages.appendChild(messageElement);
+        const messageElement = document.createElement("div");
+        messageElement.className = `chat-message-${type}`;
+        messageElement.textContent = data.message;
 
-		while (this.chatMessages.children.length > this.messageLimit)
-			this.chatMessages.removeChild(this.chatMessages.firstChild!);
+        if (!this.chatHistory.has(data.fromName))
+            this.chatHistory.set(data.fromName, []);
+        this.chatHistory.get(data.fromName)!.push(messageElement);
 
-		this.chatMessages.scrollTop = this.chatMessages.scrollHeight;
-	}
+        if (this.chatName.textContent != data.fromName && type != "outgoing")
+            return;
+        this.chatMessages.appendChild(messageElement);
 
-	addClients = (clients: { id: string, name: string }[]) => {
-		clients.forEach(client => {
-			const listItem = document.createElement("li");
-			listItem.dataset.clientId = client.id;
-			listItem.dataset.clientName = client.name
-			listItem.textContent = client.name;
-			listItem.className = "cursor-pointer p-2 hover:bg-gray-200";
-			listItem.onclick = () => this.openChat(client);
-			this.clientList.appendChild(listItem);
-		});
-	}
+        while (this.chatMessages.children.length > this.messageLimit)
+            this.chatMessages.removeChild(this.chatMessages.firstChild!);
 
-	removeClient = (client: { id: string, name: string }) => {
-		this.clientList.querySelectorAll("li").forEach((item) => {
-			const clientId = item.dataset.clientId;
-			if (client.id == clientId){
-				item.remove();
-			}
-		});
-		console.log("Clients removed");
-	}
+        this.chatMessages.scrollTop = this.chatMessages.scrollHeight;
+    };
 
-	openChat(client: { id: string, name: string }) {
-		this.chatName.textContent = `${client.name}`;
-		this.chatName.dataset.id = client.id
-		this.chatMessages.innerHTML = "";
-		this.chatInput.value = '';
-		if (!this.chatHistory.has(client.id)) {
-			this.chatHistory.set(client.id, []);
-		}
-		this.chatHistory.get(client.name)?.forEach((msg) => {
-			this.chatMessages.appendChild(msg);
-		});
-		this.chatMessages.scrollTop = this.chatMessages.scrollHeight;
-		console.log("Chat opened");
-	}
+    addClients = (clients: { id: string; name: string }[]) => {
+        clients.forEach((client) => {
+            const listItem = document.createElement("li");
+            listItem.dataset.clientId = client.id;
+            listItem.dataset.clientName = client.name;
+            listItem.textContent = client.name;
+            listItem.className = "cursor-pointer p-2 hover:bg-gray-200";
+            listItem.onclick = () => this.openChat(client);
+            this.clientList.appendChild(listItem);
+        });
+    };
 
-	onDestroy(): void {
-		Socket.emit("unsubscribe-chat");
-		Socket.emit("unsubscribe-clients");
-		Socket.removeEventListener("chat-message", this.addMessage);
-		Socket.removeEventListener("client-arrival", this.addClients);
-		Socket.removeEventListener("client-departure", this.removeClient);
-		this.chatMessages.innerHTML = "";
-	}
+    removeClient = (client: { id: string; name: string }) => {
+        this.clientList.querySelectorAll("li").forEach((item) => {
+            const clientId = item.dataset.clientId;
+            if (client.id == clientId) {
+                item.remove();
+            }
+        });
+        console.log("Clients removed");
+    };
+
+    openChat(client: { id: string; name: string }) {
+        this.chatName.textContent = `${client.name}`;
+        this.chatName.dataset.id = client.id;
+        this.chatMessages.innerHTML = "";
+        this.chatInput.value = "";
+        if (!this.chatHistory.has(client.id)) {
+            this.chatHistory.set(client.id, []);
+        }
+        this.chatHistory.get(client.name)?.forEach((msg) => {
+            this.chatMessages.appendChild(msg);
+        });
+        this.chatMessages.scrollTop = this.chatMessages.scrollHeight;
+        console.log("Chat opened");
+    }
+
+    onDestroy(): void {
+        Socket.removeEventListener("chat-message", this.addMessage);
+        Socket.removeEventListener("client-arrival", this.addClients);
+        Socket.removeEventListener("client-departure", this.removeClient);
+        Socket.removeEventListener("disconnect", this.disconnect);
+        this.chatMessages.innerHTML = "";
+    }
 }
 
 customElements.define("chat-component", Chat);
